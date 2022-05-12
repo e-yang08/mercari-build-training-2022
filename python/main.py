a574c2dca7aaeda78db58e29daa72017d3f2f4e7
e-yang08 <95173202+e-yang08@users.noreply.github.com>
2022-05-12 15:32:58 -0700

    Update main.py

--- python/main.py
+++ python/main.py
@@ -23,8 +23,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-<<<<<<< Updated upstream
-=======
+
 # ----endpoints--------------------------
 
 
@@ -42,7 +41,6 @@ def initialize():
     con.commit()
     con.close()
 
->>>>>>> Stashed changes
 
 @app.get("/")
 def root():
@@ -51,7 +49,6 @@ def root():
 
 
 @app.post("/items")
-<<<<<<< Updated upstream
 def add_item(name: str = Form(...), category: str = Form(...)):
     logger.info(f"Receive item: {name} in {category}")
 
@@ -76,64 +73,13 @@ def add_item(name: str = Form(...), category: str = Form(...)):
         json.dump(file_data, file, indent=4)
 
     return {"message": f"item received: {name} in {category}"}
-=======
-def add_item(id: int = Form(...), name: str = Form(...), category: str = Form(...)):
-    logger.info(f"Receive item - id: {id}, name:{name}, category:{category}")
-
-    con = sqlite3.connect('../db/mercari.sqlite3')
-    cur = con.cursor()
-
-    # insert item
-    cur.execute("INSERT INTO items(id, name, category) VALUES(?,?,?)",
-                (id, name, category))
-    con.commit()
-    con.close()
-
-    # For Step 3-3
-    # # open the json file to record new item defined above
-    # with open("items.json", "r+", encoding='utf-8') as file:
-    #     # load the existing data
-    #     file_data = json.load(file)
-
-    #     # define a item to be added
-    #     new_item = {
-    #         "name": name,
-    #         "category": category
-    #     }
-
-    #     # append new item to the existing items
-    #     file_data["items"].append(new_item)
-
-    #     # sets file's current position at offset.
-    #     file.seek(0)
-
-    #     # write updated data to json file
-    #     json.dump(file_data, file, indent=4)
-
-    return {f"message: item received: ID{id} - {name} in {category}"}
->>>>>>> Stashed changes
 
 
 @app.get("/items")
 def get_item():
-<<<<<<< Updated upstream
     with open("items.json", "r", encoding='utf-8') as file:
         items = json.load(file)
     return items
-=======
-    # with open("items.json", "r", encoding='utf-8') as file:
-    #     items = json.load(file)
-    logger.info("Get all items")
-
-    con = sqlite3.connect('../db/mercari.sqlite3')
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("SELECT * from items")
-    items_json = {"items": cur.fetchall()}
-    con.close()
-    return items_json
->>>>>>> Stashed changes
-
 
 @app.get("/image/{items_image}")
 async def get_image(items_image):
