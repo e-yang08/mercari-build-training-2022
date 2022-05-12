383648a1ac1dbf9569f07b0bbe5680511b5f9f29
e-yang08 <youqi.y@uni.minerva.edu>
2022-05-12 15:59:35 -0700

    Check the file path and existence

--- python/main.py
+++ python/main.py
@@ -1,13 +1,11 @@
 import os
 import logging
 import pathlib
-import json  # additionally imported
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 # additionally imported
 import json
-import sqlite3
 
 # ----config----------------------------
 app = FastAPI()
@@ -23,35 +21,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-<<<<<<< Updated upstream
-=======
+json_file = pathlib.Path(__file__).parent.resolve() / "items.json"
+
 # ----endpoints--------------------------
 
 
 @app.on_event("startup")
 def initialize():
-    logger.info("Launching the app...")
+    json_file.touch(exist_ok=True)
+    logger.info("Created items.json if not initially exist")
 
-    con = sqlite3.connect('../db/mercari.sqlite3')
-    cur = con.cursor()
-
-    # update schema
-    with open("../db/items.db", "r+", encoding='utf-8') as file:
-        schema = file.read()
-    cur.execute(f"""{schema}""")
-    con.commit()
-    con.close()
-
->>>>>>> Stashed changes
 
 @app.get("/")
 def root():
     return {"message": "Welcome to Mercari Items Database"}
 
 
-
 @app.post("/items")
-<<<<<<< Updated upstream
 def add_item(name: str = Form(...), category: str = Form(...)):
     logger.info(f"Receive item: {name} in {category}")
 
@@ -76,63 +62,13 @@ def add_item(name: str = Form(...), category: str = Form(...)):
         json.dump(file_data, file, indent=4)
 
     return {"message": f"item received: {name} in {category}"}
-=======
-def add_item(id: int = Form(...), name: str = Form(...), category: str = Form(...)):
-    logger.info(f"Receive item - id: {id}, name:{name}, category:{category}")
-
-    con = sqlite3.connect('../db/mercari.sqlite3')
-    cur = con.cursor()
-
-    # insert item
-    cur.execute("INSERT INTO items(id, name, category) VALUES(?,?,?)",
-                (id, name, category))
-    con.commit()
-    con.close()
-
-    # For Step 3-3
-    # # open the json file to record new item defined above
-    # with open("items.json", "r+", encoding='utf-8') as file:
-    #     # load the existing data
-    #     file_data = json.load(file)
-
-    #     # define a item to be added
-    #     new_item = {
-    #         "name": name,
-    #         "category": category
-    #     }
-
-    #     # append new item to the existing items
-    #     file_data["items"].append(new_item)
-
-    #     # sets file's current position at offset.
-    #     file.seek(0)
-
-    #     # write updated data to json file
-    #     json.dump(file_data, file, indent=4)
-
-    return {f"message: item received: ID{id} - {name} in {category}"}
->>>>>>> Stashed changes
 
 
 @app.get("/items")
 def get_item():
-<<<<<<< Updated upstream
     with open("items.json", "r", encoding='utf-8') as file:
         items = json.load(file)
     return items
-=======
-    # with open("items.json", "r", encoding='utf-8') as file:
-    #     items = json.load(file)
-    logger.info("Get all items")
-
-    con = sqlite3.connect('../db/mercari.sqlite3')
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("SELECT * from items")
-    items_json = {"items": cur.fetchall()}
-    con.close()
-    return items_json
->>>>>>> Stashed changes
 
 
 @app.get("/image/{items_image}")
@@ -149,8 +85,3 @@ async def get_image(items_image):
         image = images / "default.jpg"
 
     return FileResponse(image)
-
-
-@app.on_event("shutdown")
-def close():
-    logger.info("Closing the app...")
